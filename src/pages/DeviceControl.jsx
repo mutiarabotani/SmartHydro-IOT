72a6dc69e750ae679ba4e36d0d7b0cb8e754c0d3
Mutiara Botani <mutiarabotami2222@gmail.com>
2026-06-18 14:23:36 +0700

    smarthydro iot

--- src/pages/DeviceControl.jsx
+++ src/pages/DeviceControl.jsx
@@ -13,9 +13,70 @@ export default function DeviceControl() {
 
         <div className="p-5">
 
-          <h1 className="text-3xl font-bold">
+          <h1 className="text-3xl font-bold mb-4">
             Device Control
           </h1>
+
+          {/* MODE SISTEM */}
+
+          <div
+            className="
+            bg-white
+            border
+            border-slate-300
+            p-5
+            "
+          >
+
+            <div className="flex justify-between items-center">
+
+              <div>
+
+                <h2 className="text-2xl font-semibold">
+                  Mode Sistem
+                </h2>
+
+                <p className="text-slate-500">
+                  Pilih mode pengendalian sistem hidroponik
+                </p>
+
+              </div>
+
+              <div className="flex gap-4">
+
+                <button
+                  className="
+                  px-8
+                  py-4
+                  bg-green-100
+                  border
+                  border-green-500
+                  rounded
+                  font-semibold
+                  "
+                >
+                  AUTO
+                </button>
+
+                <button
+                  className="
+                  px-8
+                  py-4
+                  bg-slate-100
+                  border
+                  border-slate-400
+                  rounded
+                  font-semibold
+                  "
+                >
+                  MANUAL
+                </button>
+
+              </div>
+
+            </div>
+
+          </div>
 
         </div>
 
@@ -24,62 +85,3 @@ export default function DeviceControl() {
     </div>
   );
 }
-<div
-  className="
-  bg-white
-  border
-  border-slate-300
-  p-5
-  mt-4
-  "
->
-
-  <div className="flex justify-between items-center">
-
-    <div>
-
-      <h2 className="text-2xl font-semibold">
-        Mode Sistem
-      </h2>
-
-      <p className="text-slate-500">
-        Pilih mode pengendalian sistem hidroponik
-      </p>
-
-    </div>
-
-    <div className="flex gap-4">
-
-      <button
-        className="
-        px-8
-        py-4
-        bg-green-100
-        border
-        border-green-500
-        rounded
-        font-semibold
-        "
-      >
-        AUTO
-      </button>
-
-      <button
-        className="
-        px-8
-        py-4
-        bg-slate-100
-        border
-        border-slate-400
-        rounded
-        font-semibold
-        "
-      >
-        MANUAL
-      </button>
-
-    </div>
-
-  </div>
-
-</div>
